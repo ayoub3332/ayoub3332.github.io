f44cba2bc460ba4bfe25a2631093365988104ab2
Ayoub3332 <119888749+ayoub3332@users.noreply.github.com>
2023-05-11 19:12:08 +0200

    Update.

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Hello 🌎");
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
 const btn = document.querySelector("button"); // Get the button from the page
 if (btn) { // Detect clicks on the button
   btn.onclick = function () {
@@ -19,7 +9,6 @@ if (btn) { // Detect clicks on the button
 }
 
 
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
 
 // Open file when the link in the preview is clicked
 let goto = (file, line) => {
